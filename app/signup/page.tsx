05ec8713082461297a9c604de9b7ecd5dd146665
Boosttribe <amuchechukwucaleb@gmail.com>
2026-07-09 11:11:44 +0100

    Create page.tsx
    Commit change

---
+++ app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function Signup() {
+  return (
+    <main style={{display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', height: '100vh'}}>
+      <h1>Join Bossttribe</h1>
+      <p>This page is coming soon 🔜</p>
+      <a href="/" style={{marginTop: '20px'}}>← Back Home</a>
+    </main>
+  )
+}
